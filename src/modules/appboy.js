f91f5f743ee42a7079c6de7ab3a746b24f9c97e3
parasquid <tristan.gomez@gmail.com>
2017-10-04 16:32:25 +0800

    add in the rest of common user attributes for appboy that are handled in a special way

--- src/modules/appboy.js
+++ src/modules/appboy.js
@@ -14,12 +14,38 @@ class AppboyModule {
   setSuperProperties(properties) {
     for (const [key, value] of Object.entries(properties)) {
       let methodLut = {
+        avatarImageUrl(userObject, url) {
+          userObject.setAvatarImageUrl(url);
+        },
+        country(userObject, country) {
+          userObject.setCountry(country);
+        },
+        dateOfBirth(userObject, args) {
+          // year, month, day
+          userObject.setDateOfBirth.apply(this, args);
+        },
         email(userObject, email) {
           userObject.setEmail(email);
         },
         firstName(userObject, firstName) {
           userObject.setFirstName(firstName);
-        }
+        },
+        gender(userObject, gender) {
+          userObject.setGender(gender);
+        },
+        homeCity(userObject, city) {
+          userObject.setHomeCity(city);
+        },
+        lastKnownLocation(userObject, args) {
+          // latitude, longitude, accuracy, altitude, altitudeAccuracy
+          userObject.setLastKnownLocation.apply(this, args);
+        },
+        lastName(userObject, lastName) {
+          userObject.setLastName(lastName);
+        },
+        phoneNumber(userObject, number) {
+          userObject.setPhoneNumber(number);
+        },
       }
 
       if (key in methodLut) {
